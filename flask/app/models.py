f3521d8ed18594f24704129feb072118341f8c0e
yuki yoshinari <seal0828.4477@gmail.com>
2021-08-31 16:22:14 +0900

    add production

--- flask/app/models.py
+++ flask/app/models.py
@@ -1,6 +1,5 @@
 from app import app_, db_, login_manager
 from app.db_define import User
-from flask import request, jsonify
 
 
 
@@ -9,8 +8,3 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@app_.route('/api_test', methods=['POST'])
-def api_test():
-    json_data = request.json
-    print(json_data)
-    return "success"
