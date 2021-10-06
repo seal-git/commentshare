e5fc1f3aeaf9f6ec84667bd4009d1a0dd8d03e6e
yuki yoshinari <seal0828.4477@gmail.com>
2021-10-07 00:03:27 +0900

    update

--- react/src/App.js
+++ react/src/App.js
@@ -1,29 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
+import Home from "./components/home";
 
 function App() {
-    const onclickevent = () => {
-       axios.post('/api/api_test', {data: "test"})
-                           .then(result => {
-                               console.log('success')
-                           })
-                           .catch(error => {
-                               console.log('error')
-                           })
-    }
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <button className="App-link"
-                        onClick={onclickevent}>
-                    api!
-                </button>
-            </header>
+            <Home />
         </div>
     );
 }
---
+++ react/src/components/header.js
@@ -0,0 +1,15 @@
+import News from "./news";
+
+const headerStyle = {
+    backgroundColor: 'black',
+    color: 'white'
+}
+
+function Header(){
+    return (
+        <div className={'header'} style={headerStyle}>
+            header
+        </div>
+    );
+}
+export default Header;
