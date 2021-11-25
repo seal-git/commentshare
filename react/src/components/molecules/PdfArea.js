8cc5b2c2bc8229bc015302a4567c7fb0c6deda06
yuki yoshinari <seal0828.4477@gmail.com>
2021-11-25 17:56:01 +0900

    update pdfview

---
+++ react/src/components/molecules/PdfArea.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import samplePDF from "../sample-data/naist_exam.pdf";
+import {Page, Document} from "react-pdf/dist/esm/entry.webpack";
+import {css} from "@emotion/react";
+
+const TemplateStyle = css`
+  background: coral;
+  padding: 10px;
+  canvas{
+    margin: auto;
+    
+  }
+`
+
+const BasicStyle = css`
+  ${TemplateStyle};
+`
+
+function PdfArea(props) {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({numPages}) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
+    function changePage(offset) {
+        setPageNumber(prevPageNumber => prevPageNumber + offset);
+    }
+
+    function previousPage() {
+        changePage(-1);
+    }
+
+    function nextPage() {
+        changePage(1);
+    }
+
+    return (
+        <div className={'pdf-area'} css={BasicStyle}>
+            <Document
+                file={samplePDF}
+                options={{
+                    cMapUrl: 'https://localhost/cmaps/',
+                    cMapPacked: true,
+                }}
+                onLoadSuccess={onDocumentLoadSuccess}
+            >
+
+                <Page pageNumber={pageNumber}/>
+            </Document>
+            <div>
+                <p>
+                    Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+                </p>
+                <button
+                    type="button"
+                    disabled={pageNumber <= 1}
+                    onClick={previousPage}
+                >
+                    Previous
+                </button>
+                <button
+                    type="button"
+                    disabled={pageNumber >= numPages}
+                    onClick={nextPage}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+
+    )
+}
+
+export default PdfArea;
